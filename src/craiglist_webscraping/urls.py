dfe2d6700b542213ebec0ea64d5dc58d58db4ad5
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-02-09 10:28:00 -0300

    Finishes base and new search html

--- src/craiglist_webscraping/urls.py
+++ src/craiglist_webscraping/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('search-results', new_search, name='new_search'),
 ]
--- src/craiglist_webscraping/views.py
+++ src/craiglist_webscraping/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'craiglist_webscraping/base.html')
+
+def new_search(request):
+    return render(request, 'craiglist_webscraping/new_search.html')
