61cafe48d15b1790209099f23dde2e8bc8e1447f
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-02-10 08:10:12 -0300

    Initializing webscraping logic

--- src/craiglist_webscraping/views.py
+++ src/craiglist_webscraping/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+
+from requests import get
+from requests.compat import quote_plus
+
+
+# For now I'll just follow with the same URL done in the video and after I'll try make it dynamic
+BASE_CRAIGLIST_SEARCH_URL = 'https://losangeles.craigslist.org/search/?query={}'
 
 def home(request):
     return render(request, 'craiglist_webscraping/base.html')
 
 def new_search(request):
-    return render(request, 'craiglist_webscraping/new_search.html')
+    search = request.POST['search']
+    search_url = BASE_CRAIGLIST_SEARCH_URL.format(quote_plus(search))
+    response = get(search_url)
+    data = response.text
+
+    context = {
+        'search': search,
+    }
+    return render(request, 'craiglist_webscraping/new_search.html', context)
